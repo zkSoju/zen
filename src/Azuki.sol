d5435f5d42ecc9172f4c228d92a86d1448f8d35e
Alan Le <underrated@gmail.com>
2022-03-13 20:24:04 -0700

    🚧 temporary enumerable support

--- src/Azuki.sol
+++ src/Azuki.sol
@@ -3,8 +3,9 @@
 pragma solidity 0.8.11;
 
 import "@openzeppelin/token/ERC721/ERC721.sol";
+import "@openzeppelin/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract Azuki is ERC721 {
+contract Azuki is ERC721, ERC721Enumerable {
     string private _baseTokenURI =
         "https://ikzttp.mypinata.cloud/ipfs/QmQFkLSQysj94s5GvTHPyzTxrawwtjgiiYS2TBLgrvw8CW/";
 
@@ -32,5 +33,20 @@ contract Azuki is ERC721 {
     function mint() external {
         _safeMint(msg.sender, currentSupply);
         currentSupply += 1;
+    }
+
+    function getTokenIds(address _owner)
+        public
+        view
+        returns (uint256[] memory)
+    {
+        uint256 length = balanceOf(_owner);
+        uint256[] memory tokensOfOwner = new uint256[](length);
+
+        for (uint256 i; i < length; ) {
+            tokensOfOwner[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+
+        return (tokensOfOwner);
     }
 }
